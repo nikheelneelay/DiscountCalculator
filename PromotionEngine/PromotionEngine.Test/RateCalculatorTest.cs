2c61dcc9795b7970d8bff72f9b2beca53a3c45cc
Nikheel Neelay <P10496648@capita.co.uk>
2020-09-27 20:39:18 +0530

    Added test to get price of order based on default rates

--- PromotionEngine/PromotionEngine.Test/RateCalculatorTest.cs
+++ PromotionEngine/PromotionEngine.Test/RateCalculatorTest.cs
@@ -39,6 +39,27 @@ namespace PromotionEngine.Test
 
         }
 
+        [Fact]
+        public void RateCalculator_MockData_DefaultPriceForOrder()
+        {
+            var mockPricingService = new Mock<IPricingService>();
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.A))
+                .Returns(50);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.B))
+              .Returns(30);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.C))
+              .Returns(20);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.D))
+              .Returns(15);
+
+            var rateCal = new RateCalculator(mockPricingService.Object);
+
+            var totalPrice = rateCal.CalculateRate(GetMockOrderData());
+
+            Assert.Equal(115, totalPrice);
+
+        }
+
         public Dictionary<SKUProduct, int> GetMockOrderData()
         {
             var orderData = new Dictionary<SKUProduct, int>
