612ec2741719968ed32e10898e680c2234befb8f
Nikheel Neelay <P10496648@capita.co.uk>
2020-09-27 21:08:56 +0530

    Added offers and associated tests

--- PromotionEngine/PromotionEngine.Test/OfferServiceTest.cs
+++ PromotionEngine/PromotionEngine.Test/OfferServiceTest.cs
@@ -18,5 +18,38 @@ namespace PromotionEngine.Test
             Assert.NotNull(offers);
 
         }
+
+        [Fact]
+        public void OfferService_GetOffers_OffersHasEntry()
+        {
+            var offerService = new OfferService();
+
+            var offers = offerService.GetExistingOffers(true);
+
+            Assert.True(offers.Count > 0);
+
+        }
+
+        [Fact]
+        public void OfferService_GetActiveOffers_OffersHasEntry()
+        {
+            var offerService = new OfferService();
+
+            var offers = offerService.GetExistingOffers(false);
+
+            Assert.True(offers.Count > 0);
+
+        }
+
+        [Fact]
+        public void OfferService_IncludeInactiveOffers_OffersHasEntry()
+        {
+            var offerService = new OfferService();
+
+            var offers = offerService.GetExistingOffers(true);
+
+            Assert.NotNull(offers);
+
+        }
     }
 }
--- PromotionEngine/PromotionEngine/Services/Implementation/OfferService.cs
+++ PromotionEngine/PromotionEngine/Services/Implementation/OfferService.cs
@@ -1,4 +1,5 @@
-﻿using PromotionEngine.Models.DomainModels;
+﻿using PromotionEngine.Common;
+using PromotionEngine.Models.DomainModels;
 using PromotionEngine.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,18 @@ namespace PromotionEngine.Services.Implementation
     {
         public List<OfferModel> GetExistingOffers(bool includeInactive)
         {
-            return new List<OfferModel>();
+            // TODO - Fetch offers from DB or configurable json
+            var offers = new List<OfferModel>();
+            var product1 = new OfferProductModel { ProductName = SKUProduct.A, Qty = 3 };
+            var product2 = new OfferProductModel { ProductName = SKUProduct.B, Qty = 2 };
+            var product3 = new OfferProductModel { ProductName = SKUProduct.C, Qty = 1 };
+            var product4 = new OfferProductModel { ProductName = SKUProduct.D, Qty = 1 };
+
+            offers.Add(new OfferModel { Products = new List<OfferProductModel> { product1 }, OfferPrice = 130, Priority = 1, IsActive = true });
+            offers.Add(new OfferModel { Products = new List<OfferProductModel> { product2 }, OfferPrice = 45, Priority = 1, IsActive = true });
+            offers.Add(new OfferModel { Products = new List<OfferProductModel> { product3, product4 }, OfferPrice = 30, Priority = 1, IsActive = true });
+
+            return includeInactive ? offers : offers.Where(x => x.IsActive).ToList();
         }
     }
 }
