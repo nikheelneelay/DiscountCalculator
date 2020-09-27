a749e5d797a775d16ffb4447ddf7bad543fef32f
Nikheel Neelay <P10496648@capita.co.uk>
2020-09-27 21:46:30 +0530

    Added logic for getting discounted price and edge case test for rate calculation which are failing

--- PromotionEngine/PromotionEngine.Test/RateCalculatorTest.cs
+++ PromotionEngine/PromotionEngine.Test/RateCalculatorTest.cs
@@ -1,9 +1,11 @@
 using Moq;
 using PromotionEngine.Common;
+using PromotionEngine.Models.DomainModels;
 using PromotionEngine.Services.Implementation;
 using PromotionEngine.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace PromotionEngine.Test
@@ -14,8 +16,9 @@ namespace PromotionEngine.Test
         public void RateCalculator_MockData_DecimalData()
         {
             var mockPricingService = new Mock<IPricingService>();
+            var mockOfferService = new Mock<IOfferService>();
 
-            var rateCal = new RateCalculator(mockPricingService.Object);
+            var rateCal = new RateCalculator(mockPricingService.Object, mockOfferService.Object);
 
             var totalPrice = rateCal.CalculateRate(GetMockOrderData());
 
@@ -27,8 +30,9 @@ namespace PromotionEngine.Test
         public void RateCalculator_MockData_PricingServiceCalled()
         {
             var mockPricingService = new Mock<IPricingService>();
+            var mockOfferService = new Mock<IOfferService>();
 
-            var rateCal = new RateCalculator(mockPricingService.Object);
+            var rateCal = new RateCalculator(mockPricingService.Object, mockOfferService.Object);
 
             var totalPrice = rateCal.CalculateRate(GetMockOrderData());
 
@@ -43,6 +47,7 @@ namespace PromotionEngine.Test
         public void RateCalculator_MockData_DefaultPriceForOrder()
         {
             var mockPricingService = new Mock<IPricingService>();
+            var mockOfferService = new Mock<IOfferService>();
             mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.A))
                 .Returns(50);
             mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.B))
@@ -52,12 +57,99 @@ namespace PromotionEngine.Test
             mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.D))
               .Returns(15);
 
-            var rateCal = new RateCalculator(mockPricingService.Object);
+            var rateCal = new RateCalculator(mockPricingService.Object, mockOfferService.Object);
 
             var totalPrice = rateCal.CalculateRate(GetMockOrderData());
 
             Assert.Equal(115, totalPrice);
+        }
 
+        [Fact]
+        public void RateWithDiscount_Scenario1_ExpectedOutput()
+        {
+            var mockPricingService = new Mock<IPricingService>();
+            var mockOfferService = new Mock<IOfferService>();
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.A))
+                .Returns(50);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.B))
+              .Returns(30);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.C))
+              .Returns(20);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.D))
+              .Returns(15);
+
+            mockOfferService.Setup(repo => repo.GetExistingOffers(false))
+               .Returns(GetExistingMockOffers(false));
+
+            var rateCal = new RateCalculator(mockPricingService.Object, mockOfferService.Object);
+
+            var totalPrice = rateCal.CalculateRate(GetMockOrderDataScenario1());
+
+            Assert.Equal(100, totalPrice);
+        }
+
+        [Fact]
+        public void RateWithDiscount_Scenario2_ExpectedOutput()
+        {
+            var mockPricingService = new Mock<IPricingService>();
+            var mockOfferService = new Mock<IOfferService>();
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.A))
+                .Returns(50);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.B))
+              .Returns(30);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.C))
+              .Returns(20);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.D))
+              .Returns(15);
+
+            mockOfferService.Setup(repo => repo.GetExistingOffers(false))
+               .Returns(GetExistingMockOffers(false));
+
+            var rateCal = new RateCalculator(mockPricingService.Object, mockOfferService.Object);
+
+            var totalPrice = rateCal.CalculateRate(GetMockOrderDataScenario2());
+
+            Assert.Equal(370, totalPrice);
+        }
+
+        [Fact]
+        public void RateWithDiscount_Scenario3_ExpectedOutput()
+        {
+            var mockPricingService = new Mock<IPricingService>();
+            var mockOfferService = new Mock<IOfferService>();
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.A))
+                .Returns(50);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.B))
+              .Returns(30);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.C))
+              .Returns(20);
+            mockPricingService.Setup(repo => repo.GetDefaultRateForSKUs(SKUProduct.D))
+              .Returns(15);
+
+            mockOfferService.Setup(repo => repo.GetExistingOffers(false))
+               .Returns(GetExistingMockOffers(false));
+
+            var rateCal = new RateCalculator(mockPricingService.Object, mockOfferService.Object);
+
+            var totalPrice = rateCal.CalculateRate(GetMockOrderDataScenario3());
+
+            Assert.Equal(280, totalPrice);
+        }
+
+        public List<OfferModel> GetExistingMockOffers(bool includeInactive)
+        {
+            // TODO - Fetch offers from DB or configurable json
+            var offers = new List<OfferModel>();
+            var product1 = new OfferProductModel { ProductName = SKUProduct.A, Qty = 3 };
+            var product2 = new OfferProductModel { ProductName = SKUProduct.B, Qty = 2 };
+            var product3 = new OfferProductModel { ProductName = SKUProduct.C, Qty = 1 };
+            var product4 = new OfferProductModel { ProductName = SKUProduct.D, Qty = 1 };
+
+            offers.Add(new OfferModel { Products = new List<OfferProductModel> { product1 }, OfferPrice = 130, Priority = 1, IsActive = true });
+            offers.Add(new OfferModel { Products = new List<OfferProductModel> { product2 }, OfferPrice = 45, Priority = 1, IsActive = true });
+            offers.Add(new OfferModel { Products = new List<OfferProductModel> { product3, product4 }, OfferPrice = 30, Priority = 1, IsActive = true });
+
+            return includeInactive ? offers : offers.Where(x => x.IsActive).ToList();
         }
 
         public Dictionary<SKUProduct, int> GetMockOrderData()
@@ -66,6 +158,45 @@ namespace PromotionEngine.Test
             {
                 { SKUProduct.A, 1},
                 { SKUProduct.B, 1},
+                { SKUProduct.C, 1},
+                { SKUProduct.D, 1}
+            };
+
+            return orderData;
+        }
+
+        public Dictionary<SKUProduct, int> GetMockOrderDataScenario1()
+        {
+            var orderData = new Dictionary<SKUProduct, int>
+            {
+                { SKUProduct.A, 1},
+                { SKUProduct.B, 1},
+                { SKUProduct.C, 1},
+                { SKUProduct.D, 0}
+            };
+
+            return orderData;
+        }
+
+        public Dictionary<SKUProduct, int> GetMockOrderDataScenario2()
+        {
+            var orderData = new Dictionary<SKUProduct, int>
+            {
+                { SKUProduct.A, 5},
+                { SKUProduct.B, 5},
+                { SKUProduct.C, 1},
+                { SKUProduct.D, 0}
+            };
+
+            return orderData;
+        }
+
+        public Dictionary<SKUProduct, int> GetMockOrderDataScenario3()
+        {
+            var orderData = new Dictionary<SKUProduct, int>
+            {
+                { SKUProduct.A, 3},
+                { SKUProduct.B, 5},
                 { SKUProduct.C, 1},
                 { SKUProduct.D, 1}
             };
--- PromotionEngine/PromotionEngine/Services/Implementation/RateCalculator.cs
+++ PromotionEngine/PromotionEngine/Services/Implementation/RateCalculator.cs
@@ -9,18 +9,75 @@ namespace PromotionEngine.Services.Implementation
 {
     public class RateCalculator : IRateCalculator
     {
-        public RateCalculator(IPricingService pricingService)
+        public RateCalculator(IPricingService pricingService, IOfferService offerService)
         {
             PricingService = pricingService;
+            OfferService = offerService;
         }
 
         public IPricingService PricingService { get; }
+        public IOfferService OfferService { get; }
 
         public decimal CalculateRate(Dictionary<SKUProduct, int> orderData)
         {
             var totalPrice = orderData.Sum(x => PricingService.GetDefaultRateForSKUs(x.Key) * x.Value);
 
             return totalPrice;
+        }
+
+        public decimal CalculateRateWithPromotions(Dictionary<SKUProduct, int> orderData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetDiscountedPrice(ref Dictionary<SKUProduct, int> orderData)
+        {
+            decimal discountedTotal = 0;
+            var offers = OfferService.GetExistingOffers(false);
+
+            foreach (var offer in offers)
+            {
+                var productConsidered = orderData.Where(x => offer.Products.Any(y => y.ProductName == x.Key && x.Value >= y.Qty)).ToList();
+
+                if (productConsidered.Count != offer.Products.Count)
+                {
+                    // for the offer to be applicable all the products condition must match
+                    continue;
+                }
+
+                if (productConsidered.Any())
+                {
+
+                    decimal noOfTimesApplicable = 0;
+
+                    // Check how many times the discount can be applied 
+                    foreach (var product in productConsidered)
+                    {
+                        var applicableQty = offer.Products.FirstOrDefault(x => x.ProductName == product.Key).Qty;
+                        var times = Math.Floor((decimal)(product.Value / applicableQty));
+                        if (noOfTimesApplicable == 0)
+                        {
+                            noOfTimesApplicable = times;
+                        }
+                        else
+                        {
+                            noOfTimesApplicable = times < noOfTimesApplicable ? times : noOfTimesApplicable;
+                        }
+                    }
+
+                    discountedTotal += offer.OfferPrice * noOfTimesApplicable;
+
+                    // Removed discounted quantity from orders
+                    foreach (var product in productConsidered)
+                    {
+                        var applicableQty = offer.Products.FirstOrDefault(x => x.ProductName == product.Key).Qty;
+                        orderData[product.Key] = (int)(product.Value - applicableQty * noOfTimesApplicable);
+                    }
+                }
+
+            }
+
+            return discountedTotal;
         }
     }
 }
