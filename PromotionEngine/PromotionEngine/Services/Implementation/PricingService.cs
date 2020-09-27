7d85167fb4672f8f4fbbe61609f2311de9bd8209
Nikheel Neelay <P10496648@capita.co.uk>
2020-09-27 20:17:01 +0530

    Added default price implementation

--- PromotionEngine/PromotionEngine/Services/Implementation/PricingService.cs
+++ PromotionEngine/PromotionEngine/Services/Implementation/PricingService.cs
@@ -1,4 +1,5 @@
-﻿using PromotionEngine.Services.Implementation;
+﻿using PromotionEngine.Common;
+using PromotionEngine.Services.Implementation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace PromotionEngine.Services.Interfaces
 {
-    public class PricingService: IPricingService
+    public class PricingService : IPricingService
     {
+        public decimal GetDefaultRateForSKUs(SKUProduct product)
+        {
+            //TODO - Re factor to fetch default prices from persistent source 
+            switch (product)
+            {
+                case SKUProduct.A:
+                    return 50;
+                case SKUProduct.B:
+                    return 30;
+                case SKUProduct.C:
+                    return 20;
+                case SKUProduct.D:
+                    return 15;
+                default:
+                    return 0;
+            }
+        }
     }
 }
--- PromotionEngine/PromotionEngine/Services/Interfaces/IPricingService.cs
+++ PromotionEngine/PromotionEngine/Services/Interfaces/IPricingService.cs
@@ -8,21 +8,6 @@ namespace PromotionEngine.Services.Implementation
 {
     public interface IPricingService
     {
-        public decimal GetDefaultRateForSKUs(SKUProduct product)
-        {
-            switch (product)
-            {
-                case SKUProduct.A:
-                    return 50;
-                case SKUProduct.B:
-                    return 30;
-                case SKUProduct.C:
-                    return 20;
-                case SKUProduct.D:
-                    return 15;
-                default:
-                    return 0;
-            }
-        }
+        public decimal GetDefaultRateForSKUs(SKUProduct product);
     }
 }
